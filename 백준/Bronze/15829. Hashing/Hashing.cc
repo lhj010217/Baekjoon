74f206178a8f043f02cce195427cbdfdef0b98d2
lhj010217 <84591332+lhj010217@users.noreply.github.com>
2023-10-18 02:42:11 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,16 +3,21 @@
 #include<cmath>
 using namespace std;
 
+
 int main() {
 	int num = 0;
-	int res = 0;
+	long long res = 0;
 	int M = 1234567891;
 	string s;
 	cin >> num;
 	cin >> s;
 	for (int i = 0; i < num; i++) {
-		res += (s[i] - 'a' + 1) * pow(31, i);
+		long long pow = 1;
+		for (int j = 0; j < i; j++) {
+			pow = pow * 31;
+			pow = pow % M;
+		}
+		res += (s[i] - 'a' + 1) * pow;
 	}
-	cout << res%M;
-
+	cout << res % M;
 }
