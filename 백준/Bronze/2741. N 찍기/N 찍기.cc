971b70216e27bd17730c169dceaf2fbfd22a83ac
lhj010217 <84591332+lhj010217@users.noreply.github.com>
2023-11-04 05:56:11 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+#include<queue>
+#include<cmath>
+#include<tuple>
+#include<stack>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << "\n";
+	}
+}
