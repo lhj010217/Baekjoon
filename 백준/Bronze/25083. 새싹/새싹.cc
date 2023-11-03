27f794cbcf05ea2407fe07a7e0c36500010e183d
lhj010217 <84591332+lhj010217@users.noreply.github.com>
2023-11-04 05:52:58 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+#include<queue>
+#include<cmath>
+#include<tuple>
+#include<stack>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+}
