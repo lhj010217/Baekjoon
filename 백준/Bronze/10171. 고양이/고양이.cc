d86494b6fc454c9b3e5ad782e65ab3ff6a7b8e31
lhj010217 <84591332+lhj010217@users.noreply.github.com>
2023-11-04 06:01:10 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+#include<queue>
+#include<cmath>
+#include<tuple>
+#include<stack>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|\n";
+}
