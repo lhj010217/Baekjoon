3153cf19fe7ca633ee46ce39f6305b8ad177b784
lhj010217 <84591332+lhj010217@users.noreply.github.com>
2025-07-31 20:47:31 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,42 +1,30 @@
 #include<iostream>
-#include<stdio.h>
-#include<queue>
-#include<cmath>
-#include<tuple>
-#include<stack>
-#include<vector>
-#include<string>
-#include<cstring>
-#include<unordered_map>
 #include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<tuple>
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	vector<int> dp;
 	
-	vector<int> v(1000001);
-	int n;
-	cin >> n;
-	v[1] = 0;
-	v[2] = 1;
-	v[3] = 1;
-	for (int i = 4; i <= n; i++) {
-		if (i % 3 == 0 && i % 2 == 0) {
-			int temp = min(v[i/3], v[i/2]);
-			v[i] = min(v[i - 1], temp) + 1;
+	int N;
+	cin >> N;
+	dp.resize(N + 1, 0);
+
+	for (int i = 2; i <= N;i++) {
+		dp[i] = dp[i - 1] + 1;
+
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i], dp[i / 2] + 1);
 		}
-		else if (i % 3 == 0 && i%2 != 0) {
-			v[i] = min(v[i-1], v[i/3]) + 1;
-		}
-		else if (i % 3 != 0 && i % 2 == 0) {
-			v[i] = min(v[i - 1], v[i/2]) + 1;
-		}
-		else {
-			v[i] = v[i - 1] + 1;
+		if (i % 3 == 0) {
+			dp[i] = min(dp[i], dp[i / 3] + 1);
 		}
 	}
 
-	cout << v[n];
+	cout << dp[N];
+	return 0;
 }
