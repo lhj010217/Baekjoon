54b70cb32001d7a9ad8eb8caa7e3b8e1e3efe3f0
lhj010217 <84591332+lhj010217@users.noreply.github.com>
2025-08-02 14:13:15 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 576 ms, Memory: 19904 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<map>
+
+using namespace std;
+
+
+int main() {
+	int N;
+	vector<int> A;
+	vector<int> LIS;
+	vector<int> path;
+	int length = 0;
+
+	cin >> N;
+	LIS.resize(N + 1);
+	path.resize(N + 1);
+	A.resize(N + 1);
+
+	for (int i = 1; i < N + 1; i++) {
+		cin >> A[i];
+
+		if (i == 1) {
+			LIS[length] = A[i];
+			path[i] = 0;
+			continue;
+		}
+
+		if (A[i] > LIS[length]) {
+			LIS[++length] = A[i];
+			path[i] = length;
+		}
+		else {
+			int pos = lower_bound(LIS.begin(), LIS.begin() + length + 1, A[i]) - LIS.begin();
+			LIS[pos] = A[i];
+			path[i] = pos;
+		}
+	}
+
+	vector<int> result;
+	int target = length;
+
+	for (int i = N; i >= 1; i--) {
+		if (path[i] == target) {
+			result.push_back(A[i]);
+			target--;
+		}
+	}
+
+	reverse(result.begin(), result.end());
+
+	cout << length + 1 << "\n";
+	for (auto& x : result) {
+		cout << x << " ";
+	}
+
+}
